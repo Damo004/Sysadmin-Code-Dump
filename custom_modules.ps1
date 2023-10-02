e930cb9ade19a3efef5048d4cd93ff8ffef71add
Damian Gale <85058576+Damo004@users.noreply.github.com>
2023-10-03 08:30:10 +1100

    Check user calendar perms in org
    Scans the user account throughout the organization for permissions on other users calendars. Will also look to make one for standard inbox shortly.

---
+++ custom_modules.ps1
@@ -0,0 +1,34 @@
+# Script used to check permissions a user has for all mailboxes in the organisation
+
+Function Get-CalendarPermission {
+<#
+.SYNOPSIS
+    This is a script used to review all calendar permissions an user has within their organization
+    
+.NOTES
+    Name: Get-CalendarPermission
+    Author: Damian Gale
+    Verion: 1.0
+    DateCreated: 2023-Sep-28
+
+.EXAMPLE
+    Get-CalendarPermission -User "example@onplatinum.com.au"
+
+.LINK
+    N/A
+#>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0
+            )]
+        [string[]] $User
+    )
+    process {
+        Get-Mailbox | ForEach-Object { Get-MailboxFolderPermission (($_.PrimarySmtpAddress.ToString())+":\Calendar") -User $user -ErrorAction SilentlyContinue} | Select-Object Identity,User,AccessRights
+    }
+}
