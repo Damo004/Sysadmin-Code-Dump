b465302fb797a0b66f0fef4719429065c51c2e5c
Damian Gale <85058576+Damo004@users.noreply.github.com>
2024-04-01 14:39:11 +1100

    Update custom_modules.ps1

--- custom_modules.ps1
+++ custom_modules.ps1
@@ -32,3 +32,36 @@ Function Get-CalendarPermission {
         Get-Mailbox | ForEach-Object { Get-MailboxFolderPermission (($_.PrimarySmtpAddress.ToString())+":\Calendar") -User $user -ErrorAction SilentlyContinue} | Select-Object Identity,User,AccessRights
     }
 }
+
+Function Get-AllMailboxPermission {
+    <#
+    .SYNOPSIS
+        This is a script used to review all mailbox permissions an user has within their organization
+        
+    .NOTES
+        Name: Get-AllMailboxPermission
+        Author: Damian Gale
+        Verion: 1.0
+        DateCreated: 2023-Oct-10
+    
+    .EXAMPLE
+        Get-AllMailboxPermission -User "example@onplatinum.com.au"
+    
+    .LINK
+        N/A
+    #>
+    
+        [CmdletBinding()]
+        param(
+            [Parameter(
+                Mandatory = $true,
+                ValueFromPipeline = $true,
+                ValueFromPipelineByPropertyName = $true,
+                Position = 0
+                )]
+            [string[]] $User
+        )
+        process {
+            Get-Mailbox | ForEach-Object { Get-MailboxPermission ($_.PrimarySmtpAddress.ToString()) -User $user -ErrorAction SilentlyContinue} | Select-Object Identity,User,AccessRights
+        }
+    }
