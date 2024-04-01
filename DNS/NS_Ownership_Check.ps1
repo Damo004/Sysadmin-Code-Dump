bd301cfbd2b483b14d9a86bbd21f6d413d8706cb
Damian Gale <85058576+Damo004@users.noreply.github.com>
2024-04-01 14:33:26 +1100

    Create NS_Ownership_Check.ps1

---
+++ DNS/NS_Ownership_Check.ps1
@@ -0,0 +1,41 @@
+# Script used to see if the DNS record is hosted by the MSP. This is helpful when given a list of domains to check and verify if the NS record resolves to your own servers.
+
+function Test-DNSRecord {
+    param(
+        [string]$Domain
+    )
+    
+    try {
+        $result = nslookup -type=NS $Domain | Where-Object { $_ -match "ns1.example.com.au" }
+        $result2 = nslookup -type=NS $Domain | Where-Object { $_ -match "ns11.example.com.au" }
+        if ($result) {
+            $dnsRecord = $result -replace '\s+', ' ' -join ' '
+            return $dnsRecord
+        } elseif ($result2) {
+            $dnsRecord = $result2 -replace '\s+', ' ' -join ' '
+            return $dnsRecord
+        } else {
+            return "$Domain is not hosted by onPlatinum"
+        }
+    } catch {
+        return "Error: $_"
+    }
+}
+
+# Load data from CSV
+$csvPath = "C:\Scripts\DNS\dns.csv"
+$outputPath = "C:\Scripts\DNS\tested_dns.csv"
+
+$domains = Import-Csv -Path $csvPath | Select-Object -ExpandProperty DNS
+
+$results = @()
+
+foreach ($domain in $domains) {
+    $output = Test-DNSRecord $domain
+    $results += [PSCustomObject]@{
+        Domain = $domain
+        DNSRecord = $output
+    }
+}
+
+$results | Export-Csv -Path $outputPath -NoTypeInformation
